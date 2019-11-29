150d52c15f2fd15c473febf1a1f35255b86ef896
Ross Reedstrom <reedstrm@gmail.com>
2019-11-29 14:50:17 -0600

    add timeout to tinyterm

--- tinyterm.py
+++ tinyterm.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
-
 import serial
+import sys
+import select
 
 logger = serial.Serial("/dev/ttyACM0", 9600)
 
+
+print('>> ', end="", flush=True)
 while True:
-    print('>> ', end="")
-    cmd = input()
-    logger.write(cmd)
-    while logger.in_waiting:
-        print(logger.read())
+    i, o, e = select.select( [sys.stdin], [], [], 1 )
+    if (i):
+        cmd = sys.stdin.readline()
+        if cmd[0] != 'O':
+            logger.write(cmd[0].encode("ascii"))
+        else:
+            logger.write(cmd.encode("ascii"))
+        print('>> ', end="", flush=True)
+    while logger.inWaiting():
+        print(logger.readline().decode(), end="")
